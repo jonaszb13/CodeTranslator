b331dfbd2a99d013dba152329b538c057a4cc711
Jonas Müller <jonas.mueller.jm@gmx.de>
2022-04-25 16:52:52 +0200

    master

--- resources/js/sync.js
+++
@@ -1,7 +0,0 @@
-function sync() {
-    let codein = document.getElementById("codeintrans");
-    let codeout = document.getElementById("codeouttrans");
-    codeout.value = codein.value;
-
-    console.log(1);
-}
--- test/test.py
+++
@@ -1,8 +0,0 @@
-Gewicht = int(input("Geben sie ihr Gewicht in kg an."))
-Größe = float(input("Geben sie ihre größe in m an"))
-Größe2 = Größe*Größe 
-GewichtGröße = Gewicht/Größe2 
-
-print (f"Ihr BMI beträt {GewichtGröße}")
-
-
