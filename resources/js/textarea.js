8106e1f883ec7433c52aada17243831ba69bd781
Jonas Müller <jonas.mueller.jm@gmx.de>
2022-04-26 23:54:58 +0200

    counter for main input filed

--- resources/js/textarea.js
+++ resources/js/textarea.js
@@ -4,3 +4,28 @@ textarea.addEventListener("keyup", e =>{
   let scHeight = e.target.scrollHeight;
   textarea.style.height = `${scHeight}px`;
 });
+
+$('#input').keyup(function () {
+  var characterCount = $(this).val().length,
+  current = $('#current_comment'),
+  maximum = $('#maximum_comment'),
+  theCount = $('#the-count_comment');
+  var maxlength = $(this).attr('maxlength');
+  var changeColor = 0.75 * maxlength;
+  current.text(characterCount);
+
+  if (characterCount > changeColor && characterCount < maxlength) {
+    current.css('color', '#FF4500');
+    current.css('fontWeight', 'bold');
+  }
+  else if (characterCount >= maxlength) {
+    current.css('color', '#B22222');
+    current.css('fontWeight', 'bold');
+  }
+  else {
+    var col = maximum.css('color');
+    var fontW = maximum.css('fontWeight');
+    current.css('color', col);
+    current.css('fontWeight', fontW);
+  }
+});
